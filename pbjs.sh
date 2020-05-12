d195f88fb3208b33a3fc32015e58dbf867055275
rajin9601 <rajin9601@naver.com>
2020-05-12 17:29:22 +0900

    change yarn to npm

--- pbjs.sh
+++ pbjs.sh
@@ -9,9 +9,9 @@
 # not a big deal.
 
 mkdir -p build
-yarn pbjs --force-message -t static-module -o build/pbjs.js \
+npx pbjs --force-message -t static-module -o build/pbjs.js \
   protos/google/protobuf/descriptor.proto \
   protos/google/protobuf/compiler/plugin.proto
-yarn pbts --no-comments -o build/pbjs.d.ts build/pbjs.js
+npx pbts --no-comments -o build/pbjs.d.ts build/pbjs.js
 
 
