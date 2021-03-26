6ec7b938d684932927879d08e9f81c64d1d88278
rajin9601 <rajin9601@naver.com>
2021-03-26 11:05:48 +0900

    dev_proto fixed

--- sample/createSample.sh
+++ sample/createSample.sh
@@ -2,5 +2,4 @@ SCRIPT_DIR=$(dirname $0)
 ROOT_DIR=$(dirname $SCRIPT_DIR)
 
 npm run-script build
-echo "change the protoc-gen-ts_proto to point at build/plugin"
-protoc --plugin=$ROOT_DIR/protoc-gen-ts_proto --ts_proto_out=$SCRIPT_DIR --proto_path $SCRIPT_DIR $SCRIPT_DIR/*.proto
+protoc --plugin=$ROOT_DIR/protoc-gen-ts_dev_proto --ts_dev_proto_out=$SCRIPT_DIR --proto_path $SCRIPT_DIR $SCRIPT_DIR/*.proto
